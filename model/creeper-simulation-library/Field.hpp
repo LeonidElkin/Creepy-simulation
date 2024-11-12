f8d0646d7a25cdabd32cd1b0ec8c1d49d54e8153
LeonidElkin <113133848+LeonidElkin@users.noreply.github.com>
2024-11-12 21:52:03 +0300

    feat: added base Field implementation

--- model/creeper-simulation-library/Field.hpp
+++ model/creeper-simulation-library/Field.hpp
@@ -2,18 +2,28 @@
 #define CREEPY_SIMULATION_FIELD_HPP
 
 #include <vector>
+#include <functional>
 
 #include "Creeper.hpp"
+#include "Utils.hpp"
 
 class Field {
     size_t X_;
     size_t Y_;
+    double r_0_;
+
+    std::function<double (Point p1, Point p2)> distance_func_;
+
     std::vector<Creeper> creepers_;
+    std::vector<unsigned> dead_creepers_;
+
+    auto shuffleCreepers();
+
 public:
     void updateField();
 
-    explicit Field(size_t height, size_t width, size_t creepersNum);
+    explicit Field(size_t height, size_t width, double r0, size_t creepersNum);
 };
 
 
-#endif //CREEPY_SIMULATION_FIELD_HPP
+#endif//CREEPY_SIMULATION_FIELD_HPP
---
+++ model/creeper-simulation-library/Utils.hpp
@@ -0,0 +1,9 @@
+#ifndef CREEPY_SIMULATION_UTILS_HPP
+#define CREEPY_SIMULATION_UTILS_HPP
+
+struct Point {
+    double x;
+    double y;
+};
+
+#endif//CREEPY_SIMULATION_UTILS_HPP
