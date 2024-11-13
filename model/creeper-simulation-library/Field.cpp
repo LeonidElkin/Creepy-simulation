da1aa0b185f3cbabb8298a91429719e573522d1f
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-11-13 16:46:58 +0300

    refactoring(Field): refactoring

--- model/creeper-simulation-library/Field.cpp
+++ model/creeper-simulation-library/Field.cpp
@@ -1,47 +1,55 @@
 #include "Field.hpp"
 
 #include <algorithm>
-#include <array>
 #include <random>
+#include <ranges>
 
 constexpr auto moving_creepers = 250;
 
 auto Field::shuffleCreepers() {
-
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist(0, creepers_.size());
-
-    std::vector<unsigned int> creepersToMoveIndices(moving_creepers);
-
-    std::ranges::generate_n(creepersToMoveIndices.begin(), moving_creepers, [&] { return dist(rng); });
-
-    return creepersToMoveIndices;
+  std::uniform_int_distribution<decltype(creepers_.size())> dist(
+      0, creepers_.size());
+  std::vector<unsigned int> creepersToMoveIndices(moving_creepers);
+  std::ranges::generate_n(creepersToMoveIndices.begin(), moving_creepers,
+                          [&] { return dist(getRandom()); });
+  return creepersToMoveIndices;
 }
 
-
-Field::Field(size_t height, size_t width, double r0, size_t creepersNum) : Y_(height), X_(width), r_0_(r0), creepers_(creepersNum), dead_creepers_(0) {}
+Field::Field(Point size, double r0, size_t creepersNum)
+    : size_(size),
+      leftDownBound_(-size.x / 2, -size.y / 2),
+      rightUpBound_(size.x / 2, size.y / 2),
+      r_0_(r0) {
+  creepers_ =
+      std::views::repeat([this] -> Point {
+        static auto x_unif =
+            std::uniform_real_distribution(leftDownBound_.x, rightUpBound_.x);
+        static auto y_unif =
+            std::uniform_real_distribution(leftDownBound_.y, rightUpBound_.y);
+        return Point(x_unif(getRandom()), y_unif(getRandom()));
+      }) |
+      std::views::transform([](auto dist) { return Creeper(dist); }) |
+      std::views::take(creepersNum) | std::ranges::to<std::vector>();
+}
 
 void Field::updateField() {
+  std::ranges::for_each(dead_creepers_, &Creeper::rebornTheDead);
 
-    for (auto i: dead_creepers_) {
-        creepers_[i].rebornTheDead();
-    }
+  dead_creepers_.clear();
 
-    dead_creepers_.clear();
+  auto creepersToMoveIndices = shuffleCreepers();
 
-    auto creepersToMoveIndices = shuffleCreepers();
+  for (auto i : creepersToMoveIndices) {
+    creepers_[i].walk();
+  }
 
-    for (auto i: creepersToMoveIndices) {
-        creepers_[i].updatePositions();
-    }
-
-    for (auto i: creepersToMoveIndices) {
-        for (int j = 0; j < creepers_.size(); ++j) {
-            if (creepers_[j].updateRelations(creepers_[i], r_0_, distance_func_) == DEAD) {
-                dead_creepers_.insert(dead_creepers_.begin(), j);
-            }
-        }
-    }
-
+  dead_creepers_ =
+      creepers_ | std::views::filter([&](auto &creeper) {
+        return bool(creepersToMoveIndices | std::views::filter([&](auto i) {
+                      return creeper.updateState(creepers_[i], distance_func_,
+                                                 r_0_) ==
+                             Creeper::State::Explodes;
+                    }));
+      }) |
+      std::ranges::to<decltype(dead_creepers_)>();
 }
--- model/creeper-simulation-library/Field.hpp
+++ model/creeper-simulation-library/Field.hpp
@@ -1,29 +1,29 @@
 #ifndef CREEPY_SIMULATION_FIELD_HPP
 #define CREEPY_SIMULATION_FIELD_HPP
 
-#include <vector>
 #include <functional>
+#include <vector>
 
 #include "Creeper.hpp"
 #include "Utils.hpp"
 
 class Field {
-    size_t X_;
-    size_t Y_;
-    double r_0_;
+  Point size_;
+  Point leftDownBound_;
+  Point rightUpBound_;
+  double r_0_;
 
-    std::function<double (Point p1, Point p2)> distance_func_;
+  std::function<double(Point p1, Point p2)> distance_func_;
 
-    std::vector<Creeper> creepers_;
-    std::vector<unsigned> dead_creepers_;
+  std::vector<Creeper> creepers_;
+  std::vector<std::reference_wrapper<Creeper>> dead_creepers_;
 
-    auto shuffleCreepers();
+  auto shuffleCreepers();
 
-public:
-    void updateField();
+ public:
+  void updateField();
 
-    explicit Field(size_t height, size_t width, double r0, size_t creepersNum);
+  explicit Field(Point size, double r0, size_t creepersNum);
 };
 
-
-#endif//CREEPY_SIMULATION_FIELD_HPP
+#endif  // CREEPY_SIMULATION_FIELD_HPP
