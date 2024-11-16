b084855e54fb93e48d82ef613d0321b464fa34cb
LeonidElkin <113133848+LeonidElkin@users.noreply.github.com>
2024-11-17 02:18:05 +0300

    fix: deleted debug info

--- controller/pybind_module.cpp
+++ controller/pybind_module.cpp
@@ -4,16 +4,7 @@
 
 namespace py = pybind11;
 
-class Bruhnya {
-  double b_;
-
- public:
-  explicit Bruhnya(double b) : b_(b) {}
-  double mult(double a) { return a * b_; }
-};
-
 PYBIND11_MODULE(field_provider, handle) {
   handle.doc() = "This is the doc";
-//  py::class_<Field>(handle, "Field").def(py::init<Point, double, size_t>());
-  py::class_<Bruhnya>(handle, "Bruh").def(py::init<double>()).def("mult", &Bruhnya::mult);
+  py::class_<Field>(handle, "Field").def(py::init<Point, double, size_t>());
 }
